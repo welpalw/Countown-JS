91391b41883cce6ece372054bd6928476c98eab0
Addison Welp <157979950+welpalw@users.noreply.github.com>
2024-04-24 23:29:06 -0400

    Add files via upload

---
+++ Countdown JS/app.js
@@ -0,0 +1,34 @@
+const months = [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December",
+];
+const weekdays = [
+"Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
+];
+
+const workshopDeadline =  document.querySelector('.workshopDeadline');
+const information = document.querySelector('.information');
+const Days = document.querySelector('.Days');
+const Hours = document.querySelector('.Hours');
+const Minutes = document.querySelector('.Minutes');
+const Seconds = document.querySelector('.Seconds');
+console.log(Days);
+console.log(Hours);
+console.log(Minutes);
+console.log(Seconds);
